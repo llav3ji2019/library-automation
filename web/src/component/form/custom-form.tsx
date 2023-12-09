e147490f0e0bb245413b4b78ec37133a50e50fae
pavel <pavelemelyanov03@mail.ru>
2023-12-09 11:10:11 +0300

    add custom-form

---
+++ web/src/component/form/custom-form.tsx
@@ -0,0 +1,19 @@
+import { Dispatch, SetStateAction } from "react";
+
+type CustomFormProps = {
+  active: boolean;
+  setActive: Dispatch<SetStateAction<boolean>>;
+  children: JSX.Element;
+}
+
+function CustomForm(props: CustomFormProps): JSX.Element {
+  return (
+    <div className={props.active ? "form active" : "form"} onClick={() => props.setActive(false)}>
+      <div className={props.active ? "form__content active" : "form__content"} onClick={e => e.stopPropagation()}>
+        {props.children}
+      </div>
+    </div>  
+  );
+}
+
+export default CustomForm;
